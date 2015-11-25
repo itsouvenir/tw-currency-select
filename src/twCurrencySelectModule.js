ea5d9a6bcda153debb2e582c6e355930d5f45cac
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2015-11-25 15:06:50 +0000

    Fix height of input glitch Fix controller input

--- src/twCurrencySelectModule.js
+++ src/twCurrencySelectModule.js
@@ -6,5 +6,5 @@ var CurrencySelectController = require('./CurrencySelectController');
 var templates = require('../build/tw-currency-select-templates');
 
 var currencySelectModule = angular.module('tw-currency-select', [templates.name]);
-currencySelectModule.controller('CurrencySelectController', CurrencySelectController);
+currencySelectModule.controller('CurrencySelectController', ['$scope', '$timeout', CurrencySelectController]);
 currencySelectModule.directive('currencySelect', ['$timeout', CurrencySelectDirective]);
